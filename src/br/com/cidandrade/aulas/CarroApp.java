872aa060a7d4ab4f84ea3e8d09aa87fb67e61fac
Cid R. Andrade <profandrade@gmail.com>
2022-10-17 12:12:55 -0300

    Versão 1.0.22.10.17 Definitiva, inclui Javadoc

--- src/br/com/cidandrade/aulas/CarroApp.java
+++ src/br/com/cidandrade/aulas/CarroApp.java
@@ -1,40 +1,43 @@
 package br.com.cidandrade.aulas;
 
+import br.com.cidandrade.aulas.classes.Carro;
+
 /**
- * Esta classe implementa uma aplicação de exemplo sobre classes
+ * Aplicação de exemplo sobre classes
  *
  * @author cidandrade
+ * @version 1.0.22.10.17 Versão definitiva
  */
 public class CarroApp {
 
     public static void main(String[] args) {
         Carro carro1 = new Carro();
-        carro1.mudaMarcha(1);
-        carro1.aumentaVelocidade(10);
-        carro1.mudaMarcha(2);
-        carro1.aumentaVelocidade(20);
-        carro1.mudaMarcha(3);
-        carro1.aumentaVelocidade(30);
-        carro1.mudaMarcha(4);
-        carro1.aumentaVelocidade(30);
-        carro1.freia(10);
-        carro1.mudaMarcha(5);
-        carro1.aumentaVelocidade(40);
-        carro1.imprimeEstadoDoCarro();
+        carro1.mudarMarcha(1);
+        carro1.aumentarVelocidade(10);
+        carro1.mudarMarcha(2);
+        carro1.aumentarVelocidade(20);
+        carro1.mudarMarcha(3);
+        carro1.aumentarVelocidade(30);
+        carro1.mudarMarcha(4);
+        carro1.aumentarVelocidade(30);
+        carro1.frear(10);
+        carro1.mudarMarcha(5);
+        carro1.aumentarVelocidade(40);
+        carro1.imprimirEstadoDoCarro();
 
         Carro carro2 = new Carro();
-        carro2.mudaMarcha(1);
-        carro2.aumentaVelocidade(10);
-        carro2.mudaMarcha(2);
-        carro2.aumentaVelocidade(15);
-        carro2.mudaMarcha(3);
-        carro2.aumentaVelocidade(20);
-        carro2.mudaMarcha(4);
-        carro2.aumentaVelocidade(20);
-        carro2.freia(15);
-        carro2.mudaMarcha(5);
-        carro2.aumentaVelocidade(30);
-        carro2.imprimeEstadoDoCarro();
+        carro2.mudarMarcha(1);
+        carro2.aumentarVelocidade(10);
+        carro2.mudarMarcha(2);
+        carro2.aumentarVelocidade(15);
+        carro2.mudarMarcha(3);
+        carro2.aumentarVelocidade(20);
+        carro2.mudarMarcha(4);
+        carro2.aumentarVelocidade(20);
+        carro2.frear(15);
+        carro2.mudarMarcha(5);
+        carro2.aumentarVelocidade(30);
+        carro2.imprimirEstadoDoCarro();
     }
 
 }
